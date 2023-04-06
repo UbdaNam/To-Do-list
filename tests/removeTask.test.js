52d99c2c0c2298b1d42cf82910c0e0996be42256
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-04-06 12:05:27 +0300

    Second test for removeTask function

--- tests/removeTask.test.js
+++ tests/removeTask.test.js
@@ -1,24 +1,49 @@
-import removeTask from "../src/modules/removeTask";
+/** @format */
+
+import removeTask from '../src/modules/removeTask.js';
 
 describe('Unit tests for remove task function', () => {
-
   test('remove one task from the task list with one task', () => {
     const currentTaskList = [
       {
-        description: "Task 1",
+        description: 'Task 1',
         completed: false,
         index: 1,
-      }
+      },
     ];
     const index = 1;
     const deletedTask = {
-      description: "Task 1",
+      description: 'Task 1',
       completed: false,
       index: 1,
     };
 
     const newTaskList = removeTask(currentTaskList, index);
 
-    expect(newTaskList).not.toContainEqual(deletedTask)
-  })
-})
+    expect(newTaskList).not.toContainEqual(deletedTask);
+  });
+
+  test('expect task with given index to be removed from tasklist', () => {
+    const anotherTaskList = [
+      {
+        description: 'Task 1',
+        completed: true,
+        index: 1,
+      },
+      {
+        description: 'Task 2',
+        completed: false,
+        index: 2,
+      },
+    ];
+    const index = 2;
+
+    const tasklist = removeTask(anotherTaskList, index);
+
+    expect(tasklist).not.toContainEqual({
+      description: 'Task 2',
+      completed: false,
+      index: 2,
+    });
+  });
+});
